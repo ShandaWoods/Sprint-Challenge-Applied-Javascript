e0c56986a3ebcf64ae1ff92cf2e3ac0d6ed98c3c
Shanda Woods <shandawoods@Shandas-Mini.lan1>
2019-11-01 13:46:41 -0400

    MVP Completed!

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,62 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const articleFetcher = () => {
+    axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then(response => {
+        console.log('response is: ', response.data);
+        cardCreator(response.data.articles)
+        return response.data;
+    }).catch(error => {
+        console.log('The data was not returned', error);
+    })
+}
+articleFetcher();
+
+function Card (content) {
+    // creating elements
+    const
+    card = document.createElement('div'),
+    headline = document.createElement('div'),
+    author = document.createElement('div'),
+    imgContainer = document.createElement('div'),
+    authorPhoto = document.createElement('img'),
+    authorName = document.createElement('span')
+
+// appending children
+card.appendChild(headline);
+card.appendChild(author);
+card.appendChild(imgContainer);
+imgContainer.appendChild(authorPhoto);
+card.authorName;
+
+// adding content to elements 
+headline.textContent = content.headline;
+author.textContent = content.author;
+authorPhoto.href = content.html_url;
+authorPhoto.textContent = content.html_url;
+authorName.textContent = `By ${content.authorName}`;
+
+// adding classNames to elements
+card.classList.add('card');
+headline.classList.add('headline');
+author.classList.add('author');
+imgContainer.classList.add('image-container');
+
+document.getElementsByClassName('cards-container')[0].appendChild(card);
+return card;
+}
+
+// get request helper function 
+
+// function to loop thorough and call dataFetcher
+const cardCreator = (data) =>{
+
+  Object.values(data).forEach( value => {
+      return value.forEach(content => {
+        return Card(content);
+      })
+  })
+
+}
